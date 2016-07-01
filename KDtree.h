b5cff1a551e8fbb1a23948a64f944ac8ce63e220
todorGaidarov <tradle.tgg@gmail.com>
2016-07-01 23:09:20 +0300

    struct

--- KDtree.h
+++ KDtree.h
@@ -1,6 +1,23 @@
 #ifndef KDTREE_H_INCLUDED
 #define KDTREE_H_INCLUDED
 
+class tree
+{
+    public:
+        tree();
+        ~tree();
 
+    private:
+        struct point
+        {
+            double xCoord;
+            double yCoord;
+
+            point(double x, double y) : xCoord(x), yCoord(y) {}
+        };
+        point *root;
+
+
+};
 
 #endif // KDTREE_H_INCLUDED
