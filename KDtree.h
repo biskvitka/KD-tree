1ced2a6f6fd3f04148cd50e7bfacc8f536962511
todorGaidarov <tradle.tgg@gmail.com>
2016-07-03 21:42:14 +0300

    prePush

--- KDtree.h
+++ KDtree.h
@@ -15,9 +15,6 @@ class Tree
 
             Point(double x, double y) : xCoord(x), yCoord(y) {}
 		};
-
-<<<<<<< HEAD
-=======
 		// Node's structure (element of the tree)
 		struct Node{
 			Point data;
@@ -26,7 +23,6 @@ class Tree
 
 		Node *root;
 
->>>>>>> b9061b668eadc707e62c845b46b4a2d228082109
 };
 
 #endif // KDTREE_H_INCLUDED
