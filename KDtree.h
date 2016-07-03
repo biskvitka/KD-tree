ef6ad1af65d858be9b9dd51b5ffd748d048a1bad
Gabi <gg_grozeva@mail.bg>
2016-07-03 21:47:42 +0300

    some changes

--- KDtree.h
+++ KDtree.h
@@ -5,9 +5,12 @@ class Tree
 {
     public:
         Tree();
+		Tree(Point..)
         ~Tree();
 
     private:
+
+		//Data type of the Node
         struct Point
         {
             double xCoord;
